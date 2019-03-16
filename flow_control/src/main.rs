e664c5619ad470519d451460cba9f6389ecb14d1
KushibikiMashu <kushibiki3413@gmail.com>
2019-03-16 23:25:43 +0900

    3-5. 「制御フロー」を追加

---
+++ flow_control/src/main.rs
@@ -0,0 +1,68 @@
+fn main() {
+    let number = 6;
+
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+
+    if number != 0 {
+        println!("number was something other than zero");
+    }
+
+    if number % 4 == 0 {
+        // 数値は4で割り切れます
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        // 数値は3で割り切れます
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        // 数値は2で割り切れます
+        println!("number is divisible by 2");
+    } else {
+        // 数値は4、3、2で割り切れません
+        println!("number is not divisible by 4, 3, or 2");
+    }
+
+    let condition = true;
+    let num = if condition {
+        5
+    } else {
+        6
+    };
+
+    println!("The value of num is: {}", num);
+
+    loop {
+        println!("again");
+        break;
+    }
+
+    let mut n = 3;
+
+    while n != 0 {
+        println!("{}!", n);
+
+        n = n - 1;
+    }
+    println!("LIFTOFF!!!");
+
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("The value is: {}", a[index]);
+        index = index + 1;
+    }
+
+    for element in a.iter() {
+        println!("The value is: {}", element);
+    }
+
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
+
+    println!("LIFTOFF!!!");
+}
