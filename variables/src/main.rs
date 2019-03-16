a2eb86ec64930a8fbf35246747e4474a61e9acc2
KushibikiMashu <kushibiki3413@gmail.com>
2019-03-16 22:03:27 +0900

    3-1.「変数と可変性」のコードを追加

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,25 @@
+const MAX_POINTS: u32 = 100_000;
+
 fn main() {
-    println!("Hello, world!");
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    println!("---------------");
+
+    let y = 5;
+
+    let y = y + 1;
+
+    let y = y * 2;
+
+    println!("The value of y is: {}", y);
+
+    println!("---------------");
+
+    let spaces = "   ";
+
+    let spaces = spaces.len();
 }
+
